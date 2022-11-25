37c653e989855fe5f9a29f29cabae72323caa63a
Daniela Fialho D Oliveira <danielafialhodoliveira@iMac-de-Daniela.local>
2022-11-25 12:07:32 -0300

    [Server] - Updating Packages

---
+++ backend/src/server.ts
@@ -0,0 +1,8 @@
+import express, { Request, Response, NextFunction } from "express";
+import { router } from "./routes";
+const app = express();
+
+const port = process.env.PORT || 3333;
+app.use(express.json());
+app.use(router);
+app.listen(port, () => console.log("Server Online"));
