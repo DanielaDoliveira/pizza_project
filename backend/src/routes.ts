462565e18532fab95ed23841b94b096b587ef4c3
Daniela Fialho D Oliveira <danielafialhodoliveira@iMac-de-Daniela.local>
2022-11-28 16:52:01 -0300

    [ Backend ] - List Categories

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,13 +1,22 @@
 import { Router, Request, Response } from "express";
+import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { ListCategoryController } from "./controllers/category/ListCategoryController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { IsAuthenticated } from './middlewares/IsAuthenticated'
 const router = Router();
 
+//#region User Routes
+  router.post('/users',new CreateUserController().handle);
+  router.post('/session', new AuthUserController().handle);
+  router.get('/me', IsAuthenticated, new DetailUserController().handle);
+ 
+//#endregion
+ 
+//#region  CATEGORY ROUTES
+  router.post('/category',IsAuthenticated, new CreateCategoryController().handle);
+  router.get('/category',IsAuthenticated, new ListCategoryController().handle );
+//#endregion
 
-router.post('/users',new CreateUserController().handle);
-router.post('/session', new AuthUserController().handle);
-
-router.get('/me', IsAuthenticated, new DetailUserController().handle);
 export { router };
