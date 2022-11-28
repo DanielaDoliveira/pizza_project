fa29a54ca7b1de338fe2b48c16869302e721008c
Daniela Fialho D Oliveira <danielafialhodoliveira@iMac-de-Daniela.local>
2022-11-28 15:58:55 -0300

    [Backend]- Get User Details

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,13 @@
 import { Router, Request, Response } from "express";
 import { AuthUserController } from "./controllers/user/AuthUserController";
-import { CreateUserController } from "./controllers/user/createUserController";
-
+import { CreateUserController } from "./controllers/user/CreateUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
+import { IsAuthenticated } from './middlewares/IsAuthenticated'
 const router = Router();
 
 
 router.post('/users',new CreateUserController().handle);
-router.post('/session', new AuthUserController().handle)
+router.post('/session', new AuthUserController().handle);
+
+router.get('/me', IsAuthenticated, new DetailUserController().handle);
 export { router };
