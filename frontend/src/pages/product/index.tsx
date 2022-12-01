b6218c5aa4797c6d0e9067126f03eb550b0643c0
Daniela Fialho D Oliveira <danielafialhodoliveira@iMac-de-Daniela.local>
2022-12-01 17:25:56 -0300

    [ Frontend ] - Get Categories

--- frontend/src/pages/product/index.tsx
+++ frontend/src/pages/product/index.tsx
@@ -4,12 +4,21 @@ import { canSSRAuth } from "../../utils/canSSRAuth";
 import styles from './styles.module.scss';
 import {FiUpload } from 'react-icons/fi'
 import { ChangeEvent, useState } from "react";
+import { setupAPIClient } from "../../services/api";
 
-export default function Product() {
+type ItemProps = [
+  id: string,
+  name: string
+]
+interface CategoryProps {
+categoryList: ItemProps[];
+}
+export default function Product( { categoryList }: CategoryProps ) {
 
   const [avatarUrl, setAvatarUrl] =useState('');
   const [imageAvatar,setImageAvatar] = useState(null);
-  
+  const [categories,setCategories] = useState(categoryList);
+  const [categorySelected,setCategorySelected] = useState(0);
   function handleFile(e : ChangeEvent<HTMLInputElement>){
     if(!e.target.files){
       return;
@@ -23,6 +32,14 @@ export default function Product() {
       setAvatarUrl(URL.createObjectURL(e.target.files[0]));
     }
   }
+  function handleChangeCategory(event){
+  
+  
+    setCategorySelected(event.target.value)
+
+  }
+
+   
   return(
   <>
     <Head>
@@ -53,20 +70,15 @@ export default function Product() {
             />
           )}
         </label>
-        <select>
-            <option>
-              Bebida
-            </option>
-            <option>
-              Pizzas Salgadas
-            </option>
-
-            <option>
-              Pizzas Doces
-            </option>
-
-          
-        </select>
+        <select value={categorySelected} onChange={handleChangeCategory} >
+                {categories.map( (item, index) => {
+                  return(
+                    <option key={item.id} value={index}>
+                      {item.name}
+                    </option>
+                  )
+                })}
+            </select>
         <input 
             type = "text" 
             placeholder = "Digite o nome do produto"
@@ -97,8 +109,13 @@ export default function Product() {
   );
 }
 
-export const getServerSideProps = canSSRAuth(async(ctx)=>{
+export const getServerSideProps = canSSRAuth(async(context)=>{
+  const apiClient = setupAPIClient(context);
+  const response = await apiClient.get("/category");
+ console.log(response.data);
 return{
-  props:{}
+  props: {
+    categoryList : response.data,
+  }
 }
 })
